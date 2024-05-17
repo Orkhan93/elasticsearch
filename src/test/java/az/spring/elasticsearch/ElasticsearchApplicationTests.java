a48d1dc4f5a43b8c0df5d981e8bc795eb93608d8
OrxanMustafayev <68545038+Orkhan93@users.noreply.github.com>
2024-05-17 11:04:30 +0400

    initial

---
+++ src/test/java/az/spring/elasticsearch/ElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package az.spring.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
